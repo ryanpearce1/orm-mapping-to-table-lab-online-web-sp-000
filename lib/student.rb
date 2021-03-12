50bc86cf95d8b1895ff28f1fcb4c14e8cec3d9d2
Ryan Pearce <ryan.pearce@key.me>
2021-03-12 18:20:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
   end
   
   def self.create_table
-    sql = SQL
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT,
@@ -22,5 +22,27 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE students
+      SQL
+  
+  DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = <<-SQL 
+    INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL 
+    
+    DB[:conn].execute(sql, self.name, self.grade)
   end
   
+  def self.create(name:, grade:)
+    student = Student.new(name, grade)
+    student.save
+    student
+  end
+  
+end

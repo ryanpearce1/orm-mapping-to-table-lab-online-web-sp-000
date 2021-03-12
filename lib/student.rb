542042a15b65d9328fd5546268eb6be783491333
Ryan Pearce <ryan.pearce@key.me>
2021-03-12 18:09:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,7 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
-  attr_accessor 
+  attr_accessor :name, :grade
+  attr_reader :id 
+  
 end

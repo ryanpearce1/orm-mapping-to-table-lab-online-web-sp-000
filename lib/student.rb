ed796c0562d43754e1be061d5723080786be681c
Ryan Pearce <ryan.pearce@key.me>
2021-03-12 18:12:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,9 @@ class Student
     CREATE TABLE IF NOT EXISTS students(
     id INTEGER PRIMARY KEY,
     name TEXT,
-    grade, TEXT)
+    grade, TEXT
     SQL 
     DB[:conn].execute(sql)
-end
+    )
+  end
+  

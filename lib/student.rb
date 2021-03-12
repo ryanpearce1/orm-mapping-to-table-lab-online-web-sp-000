5a4b0c21cb1e7905c50147c97e367a2baa074771
Ryan Pearce <ryan.pearce@key.me>
2021-03-12 18:10:20 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,8 @@ class Student
   attr_accessor :name, :grade
   attr_reader :id 
   
-  def @initialize(name, grade, id=nil)
-    
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
+    @id = id
 end
